6c12c3f669962ddf389f0ce1ba74aa235ea1eb04
Mishark-dev <67210729+Mishark-dev@users.noreply.github.com>
2021-12-04 23:10:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { count } = require("console");
 const e = require("express");
 let express = require("express");
 const { request } = require("http");
@@ -17,183 +18,100 @@ let empty_list = [];
 
 //Intilaizes array of empty cells, out of which the server picks it's move
 
-app
-  .post("/post", (req, res) => {
+app.post("/post", (req, res) => {
     let jsontext;
-
     res.header("Access-Control-Allow-Origin", "*");
     let info = JSON.parse(req.query["data"]);
     let size = info["size"];
-    for (let i = 0; i < size; i++) {
-      for (let j = 0; j < size; j++) {
-        empty_list.push("" + i + j);
+    let move_num = info["move_num"];
+
+    let win = info["win"]; //Amount of cells required to win
+    let winner = "";
+    let server_move;
+
+
+    if(move_num == 1){
+      for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+          empty_list.push("" + i + j);
+        }
       }
     }
+
+
     let player_move = info["move"];
     if (empty_list.indexOf(info["move"]) != -1) {
-      let win = info["win"]; //Amount of cells required to win
-      let winner = "TEST";
-      let player_moves = player_move[0] + player_move[1];
-
-      function chooseMove(player_vectors, server_vectors, empty) {
-        //Array of moves that would be best for the player
-        let player_best = [];
-        let server_best = [];
-
-        //If it's the first move of the game
-        if (server_vectors.length == 0) {
-          return empty[3];
-        }
-
-        player_vectors.forEach((element) => {
-          //If the vector is a point
-          if (element.magnitude == 0) {
-            let nearby = element.surrounding_points();
-            for (let i = 0; i < nearby.length; i++) {
-              if (empty.indexOf(nearby[i]) != -1) {
-                player_best.push(nearby[i]);
-                return;
-              }
-            }
-          }
-          if (
-            empty.indexOf(element.nextTailPoint) == -1 &&
-            empty.indexOf(element.nextTipPoint) == -1
-          ) {
-            return;
-          } else if (empty.indexOf(element.nextTailPoint) != -1) {
-            player_best.push(element.nextTailPoint);
-          } else {
-            player_best.push(element.nextTipPoint);
-          }
-        });
-        if (player_best.length == 0) player_best.push(empty[0]);
-
-        server_vectors.forEach((element) => {
-          if (element.magnitude == 0) {
-            let nearby = element.surrounding_points();
-            for (let i = 0; i < nearby.length; i++) {
-              if (empty.indexOf(nearby[i]) != -1) {
-                server_best.push(nearby[i]);
-                return;
-              }
-            }
-          }
-          if (
-            empty.indexOf(element.nextTailPoint) == -1 &&
-            empty.indexOf(element.nextTipPoint) == -1
-          ) {
-            return;
-          } else if (empty.indexOf(element.nextTailPoint) != -1) {
-            server_best.push(element.nextTailPoint);
-          } else {
-            server_best.push(element.nextTipPoint);
-          }
-        });
-        if (server_best.length == 0) server_best.push(empty[0]);
-
-        //Choosing
-        if (empty.length == 0) {
-          return "err";
-        }
-        if (player_moves == server_best[0]) {
-          if (player_vectors[0].magnitude > server_vectors[0].magnitude) {
-            console.log(player_best);
-            return player_best[0];
-          }
-        } else if (player_moves == player_best[0]) {
-          return server_best[0];
-        } else {
-          if (player_vectors[0].magnitude > server_vectors[0].magnitude) {
-            console.log(player_best);
-            return player_best[0];
-          } else {
-            return server_best[0];
-          }
-        }
-      }
-
+    
       console.log(
         "player move:" + "(" + player_move[0] + "," + player_move[1] + ")"
       );
-
-      //ToDO: Add a check so that the player clicks on an empty list
       //Removes the players move from the stack of empty cells and adds it to the appropriate array
       let index = empty_list.indexOf(player_move);
-      if (index > -1) {
-        empty_list.splice(index, 1);
-      }
-      addMove(player_vectors, player_move);
-
-      function filtering(v) {}
-
+      empty_list.splice(index, 1);
+      addMove(player_vectors, player_move,empty_list);
       //Filters the array so it's more managable
       player_vectors = player_vectors.filter(
         (vector, index, array) =>
           index === array.findIndex((v) => v.toString() == vector.toString())
       );
-
       player_vectors.sort((firstEl, secondEl) => {
         if (firstEl.magnitude > secondEl.magnitude) return -1;
         else if (firstEl.magnitude < secondEl.magnitude) return 1;
         else return 0;
       }); //Sorts player array in terms of highest magnitutde
 
-      let server_move = chooseMove(player_vectors, server_vectors, empty_list);
-
-      console.log(server_move);
-
-      console.log(
-        "server move:" + "(" + server_move[0] + "," + server_move[1] + ")"
-      );
-      index = empty_list.indexOf(server_move);
-      if (index > -1) {
-        empty_list.splice(index, 1);
-      }
-
-      addMove(server_vectors, server_move);
+      
+      if(empty_list.length != 0) {
+        
       server_vectors = server_vectors.filter(
         (vector, index, array) =>
           index === array.findIndex((v) => v.toString() == vector.toString())
       );
-
       server_vectors.sort((firstEl, secondEl) => {
         if (firstEl.magnitude > secondEl.magnitude) return -1;
         else if (firstEl.magnitude < secondEl.magnitude) return 1;
         else return 0;
       });
 
-      if (empty_list.length == 0) {
-        winner = "tie";
-      } else if (player_vectors[0].magnitude >= win) {
-        winner = "player";
-      } else if (server_vectors[0].magnitude >= win) {
-        winner = "server";
+      server_move = chooseMove(player_vectors, server_vectors, empty_list);
+        console.log(
+          "server move:" + "(" + server_move[0] + "," + server_move[1] + ")"
+        );
+        index = empty_list.indexOf(server_move);
+        empty_list.splice(index, 1);
+
+        addMove(server_vectors, server_move, empty_list);
+
       }
 
-      console.log("Magnitude: " + server_vectors[0].magnitude);
+      else winner = "tie";
 
-      jsontext = JSON.stringify({
-        server_move: server_move,
-        winner: winner,
-      });
-    } else {
-      if (empty_list.length == 0) {
-        winner = "tie";
-      } else if (player_vectors[0].magnitude >= win) {
-        winner = "player";
-      } else if (server_vectors[0].magnitude >= win) {
-        winner = "server";
-      }
-      jsontext = JSON.stringify({
-        server_move: "err",
-        winner: winner,
-      });
+      console.log("Player magnitutude: " + player_vectors[0].magnitude);
+      console.log("Server magnitutude: " + server_vectors[0].magnitude);
+    
     }
+    else {
+      jsontext = JSON.stringify({
+      server_move: "err",
+      winner: winner,
+    });
+    }
+
+    if (player_vectors[0].magnitude >= win) {
+      winner = "player";
+    } else if (server_vectors[0].magnitude >= win) {
+      winner = "server";
+    }
+
+    console.log("Winner : "+ winner)
+
+    jsontext = JSON.stringify({
+      server_move: server_move,
+      winner: winner,
+    });
+
     res.send(jsontext);
 
-    console.log("player vectors :" + player_vectors);
-    console.log("server vectors :" + server_vectors);
 
     console.log("---------------------------------------------------");
   })
@@ -202,82 +120,167 @@ console.log("Server is running!");
 
 //Function that adds a move to a vector in the vector array, or creates a new vector if it doesn't exist
 //ToDo: Handle "gapped" vectors, vectors that have one point missing but are still threats
-function addMove(vector_array, move) {
-  //Vectors which have had a point added to them , vectors of interest
+function addMove(vector_array, move, empty) {
+  //Vectors which have had a point added to them , vectors of interest. Every single
+  //vector in this array shares a point
   let voi = [];
-  vector_array.forEach((element) => {
-    if (element.direction == 0) {
-      let nearby = element.surrounding_points();
-      if (nearby.indexOf(move) != -1) element.addPoint(move, true);
-      console.log(element.toString());
-    }
-    if (element.nextTailPoint == move) {
-      element.addPoint(move, false);
-      voi.push(element);
-    } else if (element.nextTipPoint == move) {
-      element.addPoint(move, true);
-      voi.push(element);
-    }
-  });
-  if (voi.length == 0) {
-    vector_array.push(new Vector(move, move));
-  } else if (voi.length >= 2) {
-    voi.forEach((element) => {
-      voi.forEach((element2) => {
-        if (element.direction == element2.direction) {
-          let x_max = Math.max(
-            element.initialX,
-            element.finalX,
-            element2.initialX,
-            element2.finalX
-          );
-          let y_max = Math.max(
-            element.initialY,
-            element.finalY,
-            element2.initialY,
-            element.finalX
-          );
-          let x_min = Math.min(
-            element.initialX,
-            element.finalX,
-            element2.initialX,
-            element2.finalX
-          );
-          let y_min = Math.min(
-            element.initialY,
-            element.finalY,
-            element2.initialY,
-            element.finalX
-          );
+  //Does it extend a previous vector? If it does, grow the vector
+  vector_array.forEach(vector => {
 
-          let newvec;
-          switch (element.direction[0]) {
-            case "x":
-              newvec = new Vector("" + x_min + y_min, "" + x_max + y_min);
-              break;
-            case "y":
-              newvec = new Vector("" + x_min + y_min, "" + x_min + y_max);
-              break;
-            case "d":
-              if (element.direction == "d+") {
-                newvec = new Vector("" + x_min + y_min, "" + x_max + y_max);
-              } else {
-                newvec = new Vector("" + x_min + y_max, "" + x_max + y_min);
-              }
-              break;
-            default:
-              break;
-          }
-          vector_array.splice(vector_array.indexOf(element), 1);
-          vector_array.splice(vector_array.indexOf(element2), 1);
-          vector_array.push(newvec);
+    if(vector.direction == "0"){
+      let nearby = vector.nextPoints;
+      for (let i = 0; i < nearby.length; i++) {
+        if(empty.indexOf(nearby[i]) != -1){
+          vector.addPoint(move,true);
+          voi.push(vector);
+          break; 
         }
-      });
+      }
+      return;
+    }
+    
+    if(vector.nextPoints.indexOf(move) == 0) {
+      vector.addPoint(move,true);
+      voi.push(vector);
+    }
+    else if(vector.nextPoints.indexOf(move) == 1) {
+      vector.addPoint(move,false);
+      voi.push(vector);
+    }
+    else return;
+    
+  });
+
+  //Checks if newly added vector is a part of other vectors
+  if(voi.length==0){
+    voi.push(new Vector(move,move));
+    vector_array.push(new Vector(move,move));
+    let points_arr=[];
+    let nearby = new Vector(move,move).nextPoints;
+    
+    vector_array.forEach(element => {
+      points_arr.push(element.initialPoint);
+      points_arr.push(element.finalPoint);
     });
+  //Listen, I ran out of names so I used Jeferson
+    const jeferson = nearby.filter(value => points_arr.includes(value));
+    for (let i = 0; i < jeferson.length; i++) {
+        vector_array.push(new Vector(move,jeferson[i]));
+        voi.push(new Vector(move,jeferson[i]));
+    } 
   }
+  
+    
+  //Tests if new  vector touches any points, creating any new vectors
+    
+  let addvec;
+  voi.forEach(element => {
+    voi.forEach(element2 => {
+      if(element.direction == element2.direction && element.toString() != element2.toString()){
+        let x_max = Math.max(
+          element.initialX,
+          element.finalX,
+          element2.initialX,
+          element2.finalX
+        );
+        let y_max = Math.max(
+          element.initialY,
+          element.finalY,
+          element2.initialY,
+          element.finalX
+        );
+        let x_min = Math.min(
+          element.initialX,
+          element.finalX,
+          element2.initialX,
+          element2.finalX
+        );
+        let y_min = Math.min(
+          element.initialY,
+          element.finalY,
+          element2.initialY,
+          element.finalX
+        );
+        switch (element.direction) {
+          case "x":
+            addvec = new Vector(""+x_min+y_max,""+x_max+y_max);
+            break;
+          case "y":
+            addvec = new Vector(""+x_min+y_min,""+x_max+y_max);
+            break;
+          case "d+":
+            addvec = new Vector(""+x_min+y_min,""+x_max+y_max);
+            break;
+          case "d-":
+            addvec = new Vector(""+x_min+y_max,""+x_max+y_min);
+            break;
+          default:
+            console.log("ruh roh something when wrong");
+            break;
+        }
+
+        vector_array.splice(vector_array.indexOf(element),1);
+        vector_array.splice(vector_array.indexOf(element2),1);
+        vector_array.push(addvec);
+      }
+    });
+  });
+  console.log(vector_array.toString());
+
 }
 
-function vecAdd(vec1, vec2) {}
+//Assumes that the vectors are sorted
+function chooseMove(player_vectors, server_vectors, empty) {
+  //Array of moves that would be best for the player
+  let player_best=[];
+  let server_best=[];
+
+  //If it's the first move
+  if(server_vectors.length == 0){
+    let nearby =player_vectors[0].nextPoints;
+    for (let index = 0; index < nearby.length; index++) {
+      if(empty.indexOf(nearby[index])!= -1) return nearby[index] ;
+    }
+  }
+
+  //Determines best moves for the player
+  for (let i = 0; i < player_vectors.length; i++) {
+    for (let j = 0; j < player_vectors[i].nextPoints.length; j++) {
+      let nextPoints = player_vectors[i].nextPoints;
+      
+      console.log("next_points:" + nextPoints[j] +" empty : "+empty);
+      if(empty.indexOf(nextPoints[j]) != -1) {
+        console.log("next_points: +" +nextPoints[j]);
+        player_best.push(nextPoints[j]);
+      }
+    }
+  }
+  if(player_best.length == 0) {
+    player_best.push(empty[0]);
+  }
+  
+  
+  //Determines best moves for the server
+  for (let i = 0; i <server_vectors.length; i++) {
+    for (let j = 0; j < server_vectors[i].nextPoints.length; j++) {
+      let nextPoints = server_vectors[i].nextPoints;
+      if(empty.indexOf(nextPoints[j]) != -1) {
+        server_best.push(nextPoints[j]);
+      }
+    }
+  }
+  if(server_best.length == 0) {
+    server_best.push(empty[0])
+  }
+
+  if (player_vectors[0].magnitude >= server_vectors[0].magnitude) {
+      return player_best[0];
+  }
+  else {
+      return server_best[0];
+  }
+
+}
 
 class Vector {
   //Coordinates of the vector
@@ -295,17 +298,9 @@ class Vector {
     if (this.initialPoint == this.finalPoint) {
       return "0";
     } else if (this.initialY == this.finalY) {
-      if (parseInt(this.initialX) < parseInt(this.finalX)) {
-        return "x+";
-      } else {
-        return "x-";
-      }
+        return "x";
     } else if (this.initialX == this.finalX) {
-      if (parseInt(this.initialY) < parseInt(this.finalY)) {
-        return "y+";
-      } else {
-        return "y-";
-      }
+      return "y";
     } else if (
       (parseInt(this.finalX) - parseInt(this.initialX)) /
         (parseInt(this.finalY) - parseInt(this.initialY)) >
@@ -318,56 +313,69 @@ class Vector {
   }
 
   get magnitude() {
-    let x_1 = parseInt(this.initialX);
-    let y_1 = parseInt(this.initialY);
-    let x_2 = parseInt(this.finalX);
-    let y_2 = parseInt(this.finalY);
-
-    return Math.sqrt((x_2 - x_1) ** 2 + (y_2 - y_1) ** 2) + 1;
-  }
-
-  //Returns point needed to extend the vector from the end
-  get nextTipPoint() {
-    switch (this.direction) {
-      case "x+":
-        return "" + (parseInt(this.finalX) + 1) + this.finalY;
-      case "x-":
-        return "" + (parseInt(this.finalX) - 1) + this.finalY;
-      case "y+":
-        return "" + this.finalX + (parseInt(this.finalY) + 1);
-      case "y-":
-        return "" + this.finalX + (parseInt(this.finalY) - 1);
+    switch(this.direction){
+      case "x":
+        return Math.abs(parseInt(this.finalX) - parseInt(this.initialX))+1;
+      case "y": 
+        return Math.abs(parseInt(this.finalY) - parseInt(this.initialY))+1 ;
       case "d+":
-        return "" + (parseInt(this.finalX) + 1) + (parseInt(this.finalY) + 1);
+        return Math.abs(parseInt(this.finalX) - parseInt(this.initialX)) + 1;
       case "d-":
-        return "" + (parseInt(this.finalX) - 1) + (parseInt(this.finalY) + 1);
+        return Math.abs(parseInt(this.finalX) - parseInt(this.initialX)) +1;
       default:
-        return "0";
+        return 0;
     }
   }
 
-  //Returns point needed to extend the vector from the start
-  get nextTailPoint() {
+  //Returns point needed to extend the vector 
+  get nextPoints(){
+    let ret_arr = [];
     switch (this.direction) {
-      case "x+":
-        return "" + (parseInt(this.initialX) - 1) + this.initialY;
-      case "x-":
-        return "" + (parseInt(this.initialX) + 1) + this.initialY;
-      case "y+":
-        return "" + this.initialX + (parseInt(this.initialY) - 1);
-      case "y-":
-        return "" + this.initialX + (parseInt(this.initialY) + 1);
+      case "x":
+        if(this.finalX > this.initialX){
+          ret_arr.push(""+(parseInt(this.finalX)+1)+this.finalY);
+          ret_arr.push(""+(parseInt(this.initialX)-1)+this.finalY);
+        }
+        else{
+          ret_arr.push(""+(parseInt(this.finalX)-1)+this.finalY);
+          ret_arr.push(""+(parseInt(this.initialX)+1)+this.finalY);
+        }
+        break;
+      case "y":
+        if(this.finalY > this.initialY){
+          ret_arr.push(""+this.finalX+(parseInt(this.finalY)+1));
+          ret_arr.push(""+this.finalX+(parseInt(this.initialY)-1));
+        }
+        else{
+          ret_arr.push(""+this.finalX+(parseInt(this.finalY)-1));
+          ret_arr.push(""+this.finalX+(parseInt(this.initialY)+1));
+        }
+        break;
       case "d+":
-        return (
-          "" + (parseInt(this.initialX) - 1) + (parseInt(this.initialY) - 1)
-        );
+        if(this.finalX > this.initialX){
+          ret_arr.push(""+(parseInt(this.finalX)+1)+(parseInt(this.finalY)+1));
+          ret_arr.push(""+(parseInt(this.initialX)-1)+(parseInt(this.initialY)-1));
+        }
+        else{
+          ret_arr.push(""+(parseInt(this.finalX)-1)+(parseInt(this.finalY)-1));
+          ret_arr.push(""+(parseInt(this.initialX)+1)+(parseInt(this.finalinitialXY)+1));
+        }
+        break;
       case "d-":
-        return (
-          "" + (parseInt(this.initialX) + 1) + (parseInt(this.initialY) - 1)
-        );
+        if(this.initialX < this.finalX){
+          ret_arr.push(""+(parseInt(this.finalX)+1)+(parseInt(this.finalY)-1));
+          ret_arr.push(""+(parseInt(this.initialX)-1)+(parseInt(this.initialY)+1));
+        }
+        else{
+          ret_arr.push(""+(parseInt(this.finalX)-1)+(parseInt(this.finalY)+1));
+          ret_arr.push(""+(parseInt(this.initialX)+1)+(parseInt(this.initialY)-1));
+        }
+        break;
       default:
-        return "0";
+        ret_arr=this.surrounding_points();
+        break;
     }
+    return ret_arr;
   }
 
   //Adds a point to the vector, the "side" indicates weather to add to the tip of the vector or to the tail of the vector (true = tip)
@@ -429,3 +437,11 @@ class Vector {
     );
   }
 }
+
+
+
+
+
+
+
+
